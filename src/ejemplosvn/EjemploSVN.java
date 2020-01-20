b1c45e085ee2c70fb576fcbfb8acd1bcf69571ed
serpou <60095548+serpou@users.noreply.github.com>
2020-01-20 19:34:24 +0100

    Visualiza saludo

--- src/ejemplosvn/EjemploSVN.java
+++ src/ejemplosvn/EjemploSVN.java
@@ -16,6 +16,7 @@ public class EjemploSVN {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Hola Mundo!!");
     }
     
 }
